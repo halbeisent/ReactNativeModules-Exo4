d5967e808b45c54350f0b869c2faeae1c2055944
halbeisent <thibault.halbeisendev@gmail.com>
2021-05-11 09:22:11 +0200

    Exercice 4 terminé

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 
 export default function App() {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{flex: 1, alignItems: "center", flexDirection: "row"}}>
+      <Image style={{flex: 3, height: windowHeight/2, width: windowWidth, borderColor: "black", borderWidth: 3.5}} source={require('./assets/rover.jpg')}></Image>
+      <Text style={{flex: 1, flexWrap: "wrap", color: "red", fontSize: 20}}>Le rover opportunity</Text>
       <StatusBar style="auto" />
     </View>
   );
